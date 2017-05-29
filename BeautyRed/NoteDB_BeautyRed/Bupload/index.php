1abd96cf34eb84ca0c594c8fe58ee09538998d3d
LongNice <scoser6510@gmail.com>
2017-05-29 17:02:32 +0800

    This is my work of website and note

---
+++ BeautyRed/NoteDB_BeautyRed/Bupload/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script type = 'text/javascript'>
+		alert('非管理人員無法進入!!');
+		window.location.assign("http://longnice.comxa.com/HomePage.html")
+	</script>
+
+</head>
+<body>
+</body>
+</html>
